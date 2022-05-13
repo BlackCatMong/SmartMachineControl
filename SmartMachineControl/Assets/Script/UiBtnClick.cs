8bedf66dc2339317b1ee14a2ea26ef4d3a0c8ba0
BJW <bjo0570@naver.com>
2022-05-13 15:16:51 +0900

    WorkLotation 생성 완료

--- SmartMachineControl/Assets/Script/UiBtnClick.cs
+++ SmartMachineControl/Assets/Script/UiBtnClick.cs
@@ -7,12 +7,14 @@ using UnityEngine.XR.ARSubsystems;
 public class UiBtnClick : MonoBehaviour
 {
     public GameObject indicator;
+    public GameObject workLocation;
 
     ARRaycastManager arRaycastManager;
-    bool indicatorOn = false;
+    bool indicatorOn;
     // Start is called before the first frame update
     void Start()
     {
+        indicatorOn = false;
         indicator.SetActive(false);
 
         arRaycastManager = GetComponent<ARRaycastManager>();
@@ -22,11 +24,17 @@ public class UiBtnClick : MonoBehaviour
     void Update()
     {
         CreateLocation();
+        CreateWorkLocation();
     }
     //지정 위치에 Cube 만들기 ... (CreateCube 누를 경우 생성 위치 표시 후 클릭시 그 위치에 생성 .. )
-    void CreateCubeClick()
+    public void CreateCubeClick()
     {
         indicatorOn = !indicatorOn;
+
+        if(indicatorOn)
+            Debug.Log("indicatorOn = true");
+        else
+            Debug.Log("indicatorOn = false");
     }
     //필요한 경우 생성위치 표현용 .. 
     void CreateLocation()
@@ -35,13 +43,33 @@ public class UiBtnClick : MonoBehaviour
 
         List<ARRaycastHit> aRRaycastHits = new List<ARRaycastHit>();
 
-        if(arRaycastManager.Raycast(screenSize, aRRaycastHits, TrackableType.Planes) && AndroidActivityIndicatorStyle)
+        if(arRaycastManager.Raycast(screenSize, aRRaycastHits, TrackableType.Planes) && indicatorOn)
         {
             indicator.SetActive(true);
+
+            indicator.transform.position = aRRaycastHits[0].pose.position;
+            indicator.transform.rotation = aRRaycastHits[0].pose.rotation;
+
+            indicator.transform.position += indicator.transform.up * 0.1f; // 겹치므로 올려줌
+
         }
         else
         {
             indicator.SetActive(false);
+        }
+    }
+    void CreateWorkLocation()
+    {
+        if(indicator.activeInHierarchy && indicatorOn && Input.touchCount > 0)
+        {
+            Debug.Log("Create Location Input > 0");
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("Create Location Phase Began ...");
+                Instantiate(workLocation, indicator.transform.position, indicator.transform.rotation);
+                indicatorOn = false;
+            }
         }
     }
 
