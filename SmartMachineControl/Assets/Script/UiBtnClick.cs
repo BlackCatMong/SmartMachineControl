d30be3c66f8622c374e74741b69a97fbafdad182
BJW <bjo0570@naver.com>
2022-05-16 15:11:52 +0900

    Button 추가 및 클릭 이벤트 완료 ..

--- SmartMachineControl/Assets/Script/UiBtnClick.cs
+++ SmartMachineControl/Assets/Script/UiBtnClick.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UiBtnClick : MonoBehaviour
 {
     public GameObject indicator;
     public GameObject workAreaPrefabs;
+    public GameObject canvas;
 
     ARRaycastManager arRaycastManager;
+    
     bool indicatorOn;
     // Start is called before the first frame update
     void Start()
@@ -19,8 +23,6 @@ public class UiBtnClick : MonoBehaviour
         indicator.SetActive(false);
         
         arRaycastManager = GetComponent<ARRaycastManager>();
-
-
     }
 
     // Update is called once per frame
@@ -28,6 +30,7 @@ public class UiBtnClick : MonoBehaviour
     {
         CreateLocation();
         CreateWorkArea();
+        ButtonCheck();
     }
     //지정 위치에 Cube 만들기 ... (CreateCube 누를 경우 생성 위치 표시 후 클릭시 그 위치에 생성 .. )
     public void CreateCubeClick()
@@ -45,7 +48,7 @@ public class UiBtnClick : MonoBehaviour
         {
             indicator.SetActive(true);
 
-            indicator.transform.position = aRRaycastHits[0].pose.position;
+          indicator.transform.position = aRRaycastHits[0].pose.position;
             indicator.transform.rotation = aRRaycastHits[0].pose.rotation;
 
             indicator.transform.position += indicator.transform.up * 0.1f; // 겹치므로 올려줌
@@ -56,17 +59,31 @@ public class UiBtnClick : MonoBehaviour
             indicator.SetActive(false);
         }
     }
+    //작업 구역 생성..
     void CreateWorkArea()
     {
         if(indicator.activeInHierarchy && indicatorOn && Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            if(touch.phase == TouchPhase.Began)
+            if(touch.phase == TouchPhase.Began && !EventSystem.current.currentSelectedGameObject)
             {
                 Instantiate(workAreaPrefabs, indicator.transform.position, indicator.transform.rotation);
                 indicatorOn = false;
             }
         }
     }
-
+    //Button 눌렀을때 확인
+    void ButtonCheck()
+    {
+        if (indicatorOn)
+        {
+            GameObject canvasButton = canvas.transform.GetChild(0).gameObject;
+            canvasButton.GetComponent<Image>().color = canvasButton.GetComponent<Button>().colors.pressedColor;
+        }
+        else
+        {
+            GameObject canvasButton = canvas.transform.GetChild(0).gameObject;
+            canvasButton.GetComponent<Image>().color = canvasButton.GetComponent<Button>().colors.normalColor;
+        }
+    }
 }
--- SmartMachineControl/Assets/Script/WorkAreaController.cs
+++ SmartMachineControl/Assets/Script/WorkAreaController.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 public class WorkAreaController : MonoBehaviour
 {
+    public GameObject canvas;
+
+    GameObject clickedButton;
+    
     bool sizeChangeFlag;
     bool RotateChangeFlag;
     bool MoveFlag;
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,22 +22,58 @@ public class WorkAreaController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ButtonCheck();
     }
 
-    void SizeChange()
+    public void SizeChange()
     {
         Debug.Log("SizeChange");
         sizeChangeFlag = !sizeChangeFlag;
     }
-    void RotateChange()
+    public void Move()
+    {
+        Debug.Log("Move");
+        MoveFlag = !MoveFlag;
+    }
+    public void RotateChange()
     {
         Debug.Log("RotateChange");
         RotateChangeFlag = !RotateChangeFlag;
     }
-    void Move()
+    void ButtonCheck()
     {
-        Debug.Log("Move");
-        MoveFlag = !MoveFlag;
+        bool[] flagIndex = {sizeChangeFlag, MoveFlag, RotateChangeFlag};
+        bool flagCheck = false;
+        for (int i = 0; i < flagIndex.Length; i++)
+        {
+            GameObject canvasButton = canvas.transform.GetChild(i).gameObject;
+            if (flagIndex[i])
+            {
+                canvasButton.GetComponent<Image>().color = canvasButton.GetComponent<Button>().colors.pressedColor;
+                for (int j = 0; j < canvas.transform.childCount; j++)
+                {
+                    if (i != j)
+                    {
+                        canvasButton = canvas.transform.GetChild(j).gameObject;
+                        canvasButton.SetActive(false);
+                    }
+                }
+                flagCheck = true;
+            }
+            else
+            {
+                canvasButton.GetComponent<Image>().color = canvasButton.GetComponent<Button>().colors.normalColor;
+            
+            }
+            
+            if ((i == flagIndex.Length - 1) && !flagCheck)
+            {
+                for (int j = 0; j < canvas.transform.childCount; j++)
+                {
+                    canvasButton = canvas.transform.GetChild(j).gameObject;
+                    canvasButton.SetActive(true);
+                }
+            }
+        }
     }
 }
