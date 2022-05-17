08e4e4df5edc9220ecb7f72662358af42556d016
BJW <bjo0570@naver.com>
2022-05-17 10:59:00 +0900

    Ok Cancel 버튼 추가 .. ( 배열을 주소참조로 변경 해야할거 같음 ...)

--- SmartMachineControl/Assets/Script/WorkAreaController.cs
+++ SmartMachineControl/Assets/Script/WorkAreaController.cs
@@ -3,59 +3,105 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+
+
 public class WorkAreaController : MonoBehaviour
 {
-    public GameObject canvas;
+    public GameObject canvasActiveBtn;
+	public GameObject canvasSelectBtn;
 
-    GameObject clickedButton;
-    
-    bool sizeChangeFlag;
-    bool RotateChangeFlag;
-    bool MoveFlag;
-    
-    // Start is called before the first frame update
-    void Start()
+	Button OkBtn;
+	Button CancelBtn;
+
+	bool[] flagIndex;
+
+	bool sizeChangeFlag;
+	bool rotateChangeFlag;
+	bool moveFlag;
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
+		flagIndex = new bool[] {sizeChangeFlag, rotateChangeFlag, moveFlag};
+
+		for (int i = 0; i < flagIndex.Length; i++)
+			flagIndex[i] = false;
+
+		Debug.Log("Bool Index Count -> " + flagIndex.Length);
+		canvasSelectBtn.SetActive(false);
+
+		OkBtn = canvasSelectBtn.GetComponent<Transform>().GetChild(0).GetComponent<Button>();
+		CancelBtn = canvasSelectBtn.GetComponent<Transform>().GetChild(1).GetComponent<Button>();
     }
 
     // Update is called once per frame
     void Update()
     {
+		SaveFlagIndex();
         ButtonCheck();
+		RotateChange();
+		Move();
+		SizeChange();
     }
+	void SaveFlagIndex() // 배열을 주소값 참조로 변경해야될듯...ㅠㅠㅠㅠㅠㅠㅠ
+	{
+		flagIndex[0] = sizeChangeFlag;
+		flagIndex[1] = moveFlag; 
+		flagIndex[2] = rotateChangeFlag;
+	}
 
-    public void SizeChange()
+	public void SizeChangeBtnClick()
     {
         Debug.Log("SizeChange");
         sizeChangeFlag = !sizeChangeFlag;
     }
-    public void Move()
+    public void MoveBtnClick()
     {
         Debug.Log("Move");
-        MoveFlag = !MoveFlag;
+        moveFlag = !moveFlag;
     }
-    public void RotateChange()
+    public void RotateChangeBtnClick()
     {
         Debug.Log("RotateChange");
-        RotateChangeFlag = !RotateChangeFlag;
+        rotateChangeFlag = !rotateChangeFlag;
     }
-    void ButtonCheck()
+	public void OkBtnClick()
+	{
+		Debug.Log("Ok Button Click");
+		canvasSelectBtn.SetActive(false);
+		AllFlagOff();
+	}
+	public void CancelBtnClick()
+	{
+		Debug.Log("Cancel Button Click");
+		canvasSelectBtn.SetActive(false);
+		AllFlagOff();
+
+	}
+	void AllFlagOff()
+	{
+		for (int i = 0; i < flagIndex.Length; i++)
+		{
+			flagIndex[i] = false;
+		}
+	}
+	void ButtonCheck()	
     {
-        bool[] flagIndex = {sizeChangeFlag, MoveFlag, RotateChangeFlag};
         bool flagCheck = false;
-        for (int i = 0; i < flagIndex.Length; i++)
+		for (int i = 0; i < flagIndex.Length; i++)
         {
-            GameObject canvasButton = canvas.transform.GetChild(i).gameObject;
+            GameObject canvasButton = canvasActiveBtn.transform.GetChild(i).gameObject;
+
             if (flagIndex[i])
             {
                 canvasButton.GetComponent<Image>().color = canvasButton.GetComponent<Button>().colors.pressedColor;
-                for (int j = 0; j < canvas.transform.childCount; j++)
+                for (int j = 0; j < canvasActiveBtn.transform.childCount; j++)
                 {
                     if (i != j)
                     {
-                        canvasButton = canvas.transform.GetChild(j).gameObject;
+                        canvasButton = canvasActiveBtn.transform.GetChild(j).gameObject;
                         canvasButton.SetActive(false);
+						canvasSelectBtn.SetActive(true);
                     }
                 }
                 flagCheck = true;
@@ -68,12 +114,25 @@ public class WorkAreaController : MonoBehaviour
             
             if ((i == flagIndex.Length - 1) && !flagCheck)
             {
-                for (int j = 0; j < canvas.transform.childCount; j++)
+                for (int j = 0; j < canvasActiveBtn.transform.childCount; j++)
                 {
-                    canvasButton = canvas.transform.GetChild(j).gameObject;
+					canvasButton = canvasActiveBtn.transform.GetChild(j).gameObject;
                     canvasButton.SetActive(true);
-                }
+					canvasSelectBtn.SetActive(false);
+				}
             }
         }
     }
+	void RotateChange()
+	{
+
+	}
+	void Move()
+	{
+
+	}
+	void SizeChange()
+	{
+
+	}
 }
